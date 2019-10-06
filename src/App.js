073251c3d1eb40337b2afb18770cd83e4ca2cab3
Janssen Kuhn <noconsulate@gmail.com>
2019-10-06 11:00:22 -0700

    ok here we go getting started folks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBmQ2dG6-KuKKHP7iLSzbWOIS4ZJRDimW8",
+  authDomain: "database-example-1a740.firebaseapp.com",
+  databaseURL: "https://database-example-1a740.firebaseio.com",
+  projectId: "database-example-1a740",
+  storageBucket: "database-example-1a740.appspot.com",
+  messagingSenderId: "46272556218",
+  appId: "1:46272556218:web:4b9444ecd8180d8e426683",
+  measurementId: "G-ELJ2X47FXC"
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBmQ2dG6-KuKKHP7iLSzbWOIS4ZJRDimW8",
+  authDomain: "database-example-1a740.firebaseapp.com",
+  databaseURL: "https://database-example-1a740.firebaseio.com",
+  projectId: "database-example-1a740",
+  storageBucket: "database-example-1a740.appspot.com",
+  messagingSenderId: "46272556218",
+  appId: "1:46272556218:web:4b9444ecd8180d8e426683",
+  measurementId: "G-ELJ2X47FXC"
+};
+
+firebase.initializeApp(firebaseConfig)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
